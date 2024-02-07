4f108a6e9bc33198fc25ce3f0ac8df5b48c6c0f5
jinhyeong830 <shg02387@naver.com>
2024-02-07 12:08:07 +0900

    240207: mysql 접속 및 종료

---
+++ 10_database/setting.sql
@@ -0,0 +1,10 @@
+-- window
+cd "C:\Program Files\MySQL\MySQL Server 8.0\bin"
+mysql -u root -p -- cmd powershell
+winpty mysql -u root -p -- gitbash
+
+-- MAC
+cd /usr/local/mysql/bin 
+./ mysql -u root -p
+
+quit -- mysql 종료
