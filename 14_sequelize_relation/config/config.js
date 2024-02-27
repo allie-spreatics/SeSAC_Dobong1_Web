b375b6de499ff711ffb1e636d14498cd8597f64e
jinhyeong830 <shg02387@naver.com>
2024-02-28 08:55:09 +0900

    deploy ip

--- 14_sequelize_relation/config/config.js
+++ 14_sequelize_relation/config/config.js
@@ -11,4 +11,12 @@ const development = {
   dialect: "mysql",
 };
 
-module.exports = { development };
+const production = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: "sesac",
+  host: "118.67.132.32",
+  dialect: "mysql",
+};
+
+module.exports = { development,production };
