5776f1b1dfcffafec1bb5cc1ca9906525ca3dba4
jinhyeong830 <shg02387@naver.com>
2024-03-25 14:22:43 +0900

    240325: sass& image 사용방법

--- 23-react-style/src/App.js
+++ 23-react-style/src/App.js
@@ -1,14 +1,16 @@
 import BasicCSS from "./components/BasicCss";
 import ModuleCSS from "./components/ModuleCss";
+import Sass from "./components/Sass";
 import StyledComp from "./components/StyledComp";
 
 function App() {
   return (
     <>
       <h1>react에 style 적용하기</h1>
-      <BasicCSS />
+      {/* <BasicCSS />
       <ModuleCSS />
-      <StyledComp />
+      <StyledComp /> */}
+      <Sass />
     </>
   );
 }
